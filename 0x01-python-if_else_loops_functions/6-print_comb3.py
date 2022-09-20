b1969bc687f28a5e9d3ed0e0919d2b0bcb9efb7f
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-20 14:17:22 +0000

    Python Week 2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Prints  prints all possible different combinations of two digits. 
+"""Prints  prints all possible different combinations of two digits.
     01 and 10 are considered the same combination of the two digits 0 and 1
     Print only the smallest combination of two digits"""
-for number_i in range(0,10):
+for number_i in range(0, 10):
     for number_j in range(number_i + 1, 10):
         if number_i == 8 and number_j == 9:
             print("{}{}".format(number_i, number_j))
         else:
-            print(f"{number_i}{number_j}", end= ", ")
-            
+            print(f"{number_i}{number_j}", end=", ")
+       
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Function that checks for lowercase character."""
 def islower(c):
-    if  ord(c) >= 97 and ord(c) <= 123:
+
+    
+    if ord(c) >= 97 and ord(c) <= 123:
         return True
     else:
         return False
