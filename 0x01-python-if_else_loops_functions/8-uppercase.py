4a8016b0179b9a7d58d7db472441ce7f008cd338
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-20 14:20:53 +0000

    Python Week 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Function that returns all characters as uppercase."""
+
+
 def uppercase(str):
     for i in str:
         if  ord(i) >= 97 and ord(i) <= 123:
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Function that returns the last digit of an input."""
+
+
 def print_last_digit(number):
     last_digit = abs(number) % 10
     print(f"{last_digit}", end="")
