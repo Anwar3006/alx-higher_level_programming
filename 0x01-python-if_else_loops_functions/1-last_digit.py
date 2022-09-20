14aac42d7a5196ef1bb966c771b282ca873e075f
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-20 13:58:46 +0000

    Python Week 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,4 +10,4 @@ elif last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif 0 > last_digit < 6:
     print(f"Last digit of {number} is {last_digit} \
-    and is less than 6 and not 0")
+and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Prints all numbers from 0 to 99"""
-for number in range(0,100):
+for number in range(0, 100):
     if number == 99:
         print("{}".format(number))
     else:
-        print(f"{number:02}", end= ", ")
-   
+        print(f"{number : 02}", end = ", ")
