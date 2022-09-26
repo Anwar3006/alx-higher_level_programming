1e631824cdabc38a82dc61f51dfbd4677f6131b3
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-26 14:34:37 +0000

    Python WEEK 3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,5 @@ def element_at(my_list, idx):
     """Function that retrieves an element from a list like in C."""
 
     if idx < 0 or idx > len(my_list) - 1:
-        return 0
-    else:
-        return (my_list[idx])
+        return None
+    return (my_list[idx])
