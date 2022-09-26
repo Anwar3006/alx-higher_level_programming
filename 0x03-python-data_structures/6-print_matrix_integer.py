404df39c9717f192e5e2e525a52a89310e7f30e6
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-26 14:17:21 +0000

    Python WEEK 3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,12 +9,3 @@ def print_matrix_integer(matrix=[[]]):
                     print(" ", end="")
 
         print("")
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
