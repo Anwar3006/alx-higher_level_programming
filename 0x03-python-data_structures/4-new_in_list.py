b909275cdeaf29281a77f8957024adb1f8aa62a1
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-27 13:50:35 +0000

    Python WEEK 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Function that replaces an element of a list at a specific position (like in C)"""
+    """Function that replaces list element at a specific position(like in C)"""
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    """Function that returns a tuple with the length of a string and its first character."""
+    """Function that returns a tuple, length of string and first character."""
     if not sentence:
         return (0, None)
-    
+
     first = sentence[0]
     tup = (len(sentence), first)
     return (tup[0], tup[1])
