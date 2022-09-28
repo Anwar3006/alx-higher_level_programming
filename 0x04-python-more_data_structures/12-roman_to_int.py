56857878b962b83891fa2368003846753cee69fb
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-28 20:45:47 +0000

    Python WEEK 3-2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,39 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    for 
+    result, i = 0, 0
+
+    while i < len(roman_string):
+        s1 = value_at(roman_string[i])
+
+        if (i + 1) < len(roman_string):
+            s2 = value_at(roman_string[i + 1])
+            if s1 >= s2:
+                result = result + s1
+                i = i + 1
+            else:
+                result = result + s2 - s1
+                i = i + 2
+        else:
+            result = result + s1
+            i = i + 1
+    return result
+
+
+def value_at(idx):
+    if idx == 'M':
+        return 1000
+    if idx == 'D':
+        return 500
+    if idx == 'C':
+        return 100
+    if idx == 'L':
+        return 50
+    if idx == 'X':
+        return 10
+    if idx == 'V':
+        return 5
+    if idx == 'I':
+        return 1
+    else:
+        return 0
