93b692ba41d48640d359b4c78e697dff46f063f4
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-28 18:05:46 +0000

    Python WEEK 3-2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,10 @@
 
 def best_score(a_dictionary):
     """Function that returns a key with the biggest integer value."""
-    if(a_dictionary):
-        return (max(a_dictionary.keys()))
-    else:
+    if a_dictionary is None:
         return None
+
+    best = max(a_dictionary.values(), default=None)
+    for k, v in a_dictionary.items():
+        if v == best:
+            return k
