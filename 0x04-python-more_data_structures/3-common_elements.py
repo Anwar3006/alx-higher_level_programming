59017c873f7671e549b3a0bce3d93454e226d24a
Anwar3006 <anwarsadat.d2@gmail.com>
2022-09-28 13:05:07 +0000

    Python WEEK 3-2

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,7 +2,4 @@
 
 def common_elements(set_1, set_2):
     """Function that returns a set of common elements in two sets."""
-
-    for element in set_1 and set_2:
-        if element in set_1 and set_2:
-            return element
+    return (set_1 & set_2)
