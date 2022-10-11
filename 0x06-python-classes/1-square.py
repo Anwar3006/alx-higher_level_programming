fc0a19e581b68a186ce963c37730880ba636a074
Anwar3006 <anwarsadat.d2@gmail.com>
2022-10-11 20:32:44 +0000

    Python WEEK 4-2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        """Initializing new square
+        Args: size(int) size of new square"""
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Function to be used for getting/retrieving
+        the private instance attribute(size) value"""
+        return (self.__size)
+    
+    @size.setter
+    def size(self, value):
+        """Function to be used for setting 
+        the private instance attribute value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
