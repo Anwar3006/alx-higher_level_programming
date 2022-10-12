7e8dd192d36dda67c3de995b9730756939eaf37f
Anwar3006 <anwarsadat.d2@gmail.com>
2022-10-12 11:37:55 +0000

    Python WEEK 4-2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
         """Initializing new square
         Args: size(int) size of new square"""
