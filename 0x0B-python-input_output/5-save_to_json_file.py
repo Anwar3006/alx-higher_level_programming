5f78112a957621acf313727cd4ea072bc9c4a0d8
Anwar3006 <anwarsadat.d2@gmail.com>
2022-10-25 22:01:52 +0000

    Python File Handling

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,7 +7,7 @@ def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation.
     Args:
         my_obj (str): string object to be encoded
-        
+
     Return
         JSON string representation of my_obj"""
     with open(filename, mode='w') as f:
