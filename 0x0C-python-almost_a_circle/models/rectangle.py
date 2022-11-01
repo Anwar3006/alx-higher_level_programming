62b0d616efe365e7df62c12d291d2385550f5740
Anwar3006 <anwarsadat.d2@gmail.com>
2022-11-01 10:59:28 +0000

    Python OOP

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,8 +38,8 @@ class Rectangle(Base):
         """Setter method for private instance attribute"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -52,8 +52,8 @@ class Rectangle(Base):
         """Setter method for private instance attribute"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
