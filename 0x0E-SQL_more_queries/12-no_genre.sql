9acd6eb92d41ac855eb1ec802772fec89c1793df
Anwar3006 <anwarsadat.d2@gmail.com>
2022-11-30 18:39:37 +0000

    SQL day 2

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
 -- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
 SELECT show.`title`, gen.`genre_id`
-FROM `tv_shows` AS show
+  FROM `tv_shows` AS show
     LEFT JOIN `tv_show_genres` AS gen
     ON show.`title` = gen.`genre_id`
     WHERE gen.`genre_id` IS NULL
