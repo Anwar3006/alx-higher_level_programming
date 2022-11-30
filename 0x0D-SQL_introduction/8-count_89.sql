f76c3e767e05983511d2a23990db9b9dabb8a9cd
Anwar3006 <anwarsadat.d2@gmail.com>
2022-11-30 09:31:59 +0000

    SQL day 1

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Script that displays the number of records with id = 89 in the table first_table of the database
-SELECT COUNT(DISTINCT `id`)
+SELECT COUNT(DISTINCT *)
 FROM `first_table`
 WHERE `id` = 89;
