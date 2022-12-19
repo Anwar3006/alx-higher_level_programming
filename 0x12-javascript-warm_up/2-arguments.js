10434efd00a6bf5a7b9f50e3063aae1726307acb
Anwar3006 <anwarsadat.d2@gmail.com>
2022-12-19 23:06:04 +0000

    JavaScript Intro

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length === 3) {
+    console.log("Argument Found");
+}
+else if (process.argv.length === 2)
+{
+    console.log("No argument");
+}
+else 
+{
+    console.log("Arguments found");
+}
+
