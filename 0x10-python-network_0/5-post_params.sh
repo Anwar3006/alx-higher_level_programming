eee98b3c8a6216932d87dc7a8cff10fe7c6acf13
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-16 19:08:48 +0000

    Networking_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL- send two variables email & subject with their values
-curl -s -X POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
+curl -s "$1" -X POST -d "email:test@gmail.com&subject:I will always be here for PLD"
