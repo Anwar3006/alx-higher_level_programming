5eccad7464ec57e3501760853f7077a33fc59fa5
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-16 17:16:40 +0000

    Networking_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for a given URL.
-if [ $# -lt 1 ]
-then
-    echo "No URL provided"
-else
-    curl -s "$1" | wc -c
-fi
+
+curl -s "$1" | wc -c
