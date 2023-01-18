2b5d560d1695793951b7177e522d51a5fffcd370
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-18 19:40:20 +0000

    Networking_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    q = argv[1]
+    if len(argv[1]) > 1:
+        q = argv[1]
     if q == "" or type(q) != str:
         print('No result')
 
