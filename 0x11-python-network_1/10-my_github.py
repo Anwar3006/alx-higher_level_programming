1865c3f71748ceb81c73979043d4cf820869b980
Anwar3006 <anwarsadat.d2@gmail.com>
2023-01-18 19:17:38 +0000

    Networking_1

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Takes my Github credentials (username and token)
+and uses the Github API to display my Github id."""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    git_uri = 'https://api.github.com/users/' + username
+
+    response = requests.get(git_uri, auth=(username, token))
+
+    data = response.json()
+
+    print(data['id'])
