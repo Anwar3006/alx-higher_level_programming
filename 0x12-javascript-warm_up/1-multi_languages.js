105ea9b463de8315c7f548c9697c332f7085d25a
Anwar <anwarsadat.d2@gmail.com>
2023-03-12 16:54:16 +0000

    JavaScript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for (let i = 0; i < 3; i++)
-{
-    console.log(myVar[i]);
-}
+console.log('C is fun\nPython is cool\nJavascript is amazing');
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for (let i = 0; i < 3; i++)
-{
-    console.log(myVar[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
